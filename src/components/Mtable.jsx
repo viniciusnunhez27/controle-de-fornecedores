9f832a525d76e539e94715f33fcdb38542c586f2
vinicius <v.nunhez27@gmail.com>
2022-08-22 19:11:59 -0300

    consumindo API

--- src/components/Mtable.jsx
+++ src/components/Mtable.jsx
@@ -1,20 +1,78 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
 
 
-const [fornecedores,setFornecedores] = useState([]);
-const url = 'http://localhost:3000/fornecedores';
 
+function Mtable() {
 
+const [fornecedores, setFornecedores] = useState([]);
 
 useEffect(() => {
     fetch("http://localhost:3000/fornecedores")
     .then(response  => response.json())
     .then(data => {
-      setFornecedores(data)  
-      console.log(data) 
+      setFornecedores(data) 
     })
 }, [])
 
 
+  return (
+    <TableContainer component={Paper}>
+    <Table sx={{ minWidth: 1000}} aria-label="customized table">
+      <TableHead>
+        <TableRow>
+          <StyledTableCell>Nome</StyledTableCell>
+          <StyledTableCell align="right">Pais</StyledTableCell>
+          <StyledTableCell align="right">Estado</StyledTableCell>
+          <StyledTableCell align="right">Telefone)</StyledTableCell>
+          <StyledTableCell align="right">Ramo</StyledTableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {fornecedores.map((fornecedor) => (
+          <StyledTableRow key={fornecedores.id}>
+          <StyledTableCell component="th" scope="row">
+          {fornecedor.name}
+          </StyledTableCell>
+          <StyledTableCell align="right">{fornecedor.pais}</StyledTableCell>
+          <StyledTableCell align="right">{fornecedor.estado}</StyledTableCell>
+          <StyledTableCell align="right">{fornecedor.telefone}</StyledTableCell>
+          <StyledTableCell align="right">{fornecedor.ramo}</StyledTableCell>
+        </StyledTableRow>
+      ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+ );
+}
+  
+    
 
-export default Mtable
+export default Mtable;
