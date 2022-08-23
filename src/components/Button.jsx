50797038eb307f05822e88117340d7f580cb00e9
vinicius <v.nunhez27@gmail.com>
2022-08-23 20:58:44 -0300

    button color ativo e inativo

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import styles from './Button.css'
 import Mtable from './Mtable'
-
+import { useState } from 'react';
 
 
 
 function Button( props ) {
+
+
+const  [botaoInativo, setBotaoInativo] = useState(false);
+
+
+const inativarBotao = () => {
+  setBotaoInativo(!botaoInativo)
+
+}
+
   return (
     <div>
-         <button className='btn'>
-        {props.text}</button>
+         <button className='btn' data-active={botaoInativo} onClick={inativarBotao} >
+         {botaoInativo ? 'Ativo': 'Inativo'}   
+        </button>
     </div>
   )
 }
